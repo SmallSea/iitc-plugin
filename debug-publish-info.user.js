3cee6dc01900ee51e97c2e7d032cae10d6659abc
SmallSea <smallsea@gmail.com>
2017-07-17 14:38:39 +0800

    Added Mods for new Ito En Transmuter.

--- debug-publish-info.user.js
+++ debug-publish-info.user.js
@@ -3,7 +3,7 @@
 // @name           IITC plugin: Publish well-formed portal information
 // @author         SmallSea
 // @category       Debug
-// @version        0.1.1.2
+// @version        0.1.1.3
 // @namespace      pInfo
 // @updateURL      https://github.com/SmallSea/iitc-plugin/raw/master/debug-publish-info.user.js
 // @downloadURL    https://github.com/SmallSea/iitc-plugin/raw/master/debug-publish-info.user.js
@@ -27,7 +27,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'jonatkins';
-plugin_info.dateTimeVersion = '0.1.0.2';
+plugin_info.dateTimeVersion = '0.1.0.3';
 plugin_info.pluginId = 'debug-publish-info';
 //END PLUGIN AUTHORS NOTE
 
@@ -101,6 +101,12 @@ window.plugin.pInfo.showPortalData = function(guid) {
               break;
           case "AXA Shield":
               mod = 'AXA';
+              break;
+          case "Ito En Transmuter (+)":
+              mod = 'I+';
+              break;
+          case "Ito En Transmuter (-)":
+              mod = 'I-';
               break;
           case "SoftBank Ultra Link":
               mod = 'SULA';
